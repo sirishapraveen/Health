220b7b213be36bf2804369981873d6e3a6a9efc3
sirishapraveen <sirishapraveen.sp@gmail.com>
2025-04-12 11:26:24 -0400

    Update Medical.java

--- Medical.java
+++ Medical.java
@@ -3,5 +3,7 @@ pubic class Book medicine
     public static void main (String []args)
     {
       System.out.println("i am good girl");
+      System.out.println("i am good girl");
+      
     }
   }
